90b258549ace90c12e8484df859ffad6982b024c
OldBrain <hell_brain@mail.ru>
2021-07-08 20:16:39 +0400

    Полностью переработан клиент добавлена Map убраены ветвления, добавлен класс Factory.

--- cloud-client/src/main/java/cloudclient/front/controlers/AuthorizationController.java
+++ cloud-client/src/main/java/cloudclient/front/controlers/AuthorizationController.java
@@ -12,7 +12,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
